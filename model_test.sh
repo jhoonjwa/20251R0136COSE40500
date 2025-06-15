93ba9f589a9a509948f8b63fc222b05177462ac0
Jonghoon <alphabet1@korea.ac.kr>
2025-06-15 14:43:37 +0000

    script minor fix

--- model_test.sh
+++ model_test.sh
@@ -2,7 +2,7 @@ python /home/ubuntu/jonghoon/AI-TA/test_models.py
 sleep 3
 CUDA_VISIBLE_DEVICES=0 deepspeed /home/ubuntu/jonghoon/AI-TA/train/train_dpo.py
 
-sleep 3600
+sleep 3
 
 sleep 3
 CUDA_VISIBLE_DEVICES=0, 1 deepspeed /home/ubuntu/jonghoon/AI-TA/train/train_dpo.py
